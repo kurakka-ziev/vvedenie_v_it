e66039ff0f1df272423bb36a3b9c9f35f2dda39c
kurakka-ziev <z.kurakka@gmail.com>
2022-09-30 16:16:57 +0300

    render errors

--- quadric.py
+++ quadric.py
@@ -1,9 +1,19 @@
-a = int(input('a = '))
-b = int(input('b = '))
-c = int(input('c = '))
+s = 0
+while s == 0: 
+    try: 
+        a = int(input('a = '))
+        if a == 0:
+            print("don't make a = 0 pls \n")
+            continue
+        b = int(input('b = '))
+        c = int(input('c = '))
+    except:
+        print('Vvedite chislo, a ne da')
+        continue
+    s = 1
 
 d = b**2 - 4*a*c
-
+print("d = ", d)
 # ответы с нецелыми корнями округляются
 if d > 0:
 	x1 = (-b + (d **1/2)) / (2 * a) 
